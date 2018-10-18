c9b86576476169eb4bdb895d47505790837db0e7
hongqun <hongqun@126.com>
2018-10-18 22:56:18 +0800

    add ui test

--- PhotoCollecter/src/UI/imgviewer.py
+++ PhotoCollecter/src/UI/imgviewer.py
@@ -1,15 +1,25 @@
 from Ui_ImgViewer import Ui_ImgViewer
-from PyQt5.QtWidgets import QMainWindow,QFileDialog
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QMainWindow,QFileDialog,QFileSystemModel
+from PyQt5.QtCore import pyqtSlot,QDir
 class ImgViewer(QMainWindow,Ui_ImgViewer):
     def __init__(self):
         super(QMainWindow,self).__init__()
         self.setupUi(self)
+        self.rootdir = None
+        self.model = QFileSystemModel()
+        self.treeView.setModel(self.model)
+        self.treeView.clicked.connect(self.on_file_clicked)
 
+    @pyqtSlot()
+    def on_file_clicked(self,*index):
+        print(index)
 
     @pyqtSlot()
     def on_actionOpen_triggered(self):
         dialog = QFileDialog()
-        result = dialog.exec_()
-        if result == QFileDialog.Accepted:
-            print(dialog.selectedFiles()[0])
+        result = dialog.getExistingDirectory()
+        self.rootdir = result
+        self.model.setRootPath(self.rootdir)
+        self.treeView.setRootIndex(self.model.index(self.rootdir))
+
+
